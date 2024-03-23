a628075b34444b8870d034c9e3d7902ce75b0b8f
juanwall <john+github@wall.cyou>
2024-03-23 17:37:33 -0400

    Update edit URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/lemursman/fullup-docs/tree/master/docs/',
+          editUrl: 'https://github.com/lemursman/fullup-docs/tree/master/',
         },
         theme: {
           customCss: './src/css/custom.css',
